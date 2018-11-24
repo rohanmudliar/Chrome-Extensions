3edfaa6b4e68b6936a9d0c00a325620050ceaa9a
Rohan Mudliar <rohan.mudliar@gmail.com>
2018-11-24 23:04:48 +0530

    added extension for drawing on the screen

---
+++ Chrome Extension for drawing/script.js
@@ -0,0 +1,23 @@
+console.log("Sketch is running");
+
+var s = function( sketch ) {
+
+  sketch.setup = function() {
+      document.body.style['user-select']='none';
+      let h = document.body.clientHeight;
+    let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+    c.position(0, 0);
+    c.style('pointer-events', 'none');
+    sketch.clear();
+  };
+
+  sketch.draw = function() {
+    sketch.stroke(255,0,0);
+    sketch.strokeWeight(4);
+    if(sketch.mouseIsPressed) {
+        sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+    }
+  }
+};
+
+var myp5 = new p5(s);
